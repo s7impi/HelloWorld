faf279abd67116595b8cc9144857cbfd5e5c536c
vitalik <vx0@yandex.ru>
2015-03-19 11:34:23 +0100

    Test message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!, tu byl Witek");
+        System.out.println("Hello World!, tu byl Witek2");
     }
 }
