ea7946a380a503f647470f6cffe981dcf6b3b100
vitalik <vx0@yandex.ru>
2015-03-24 22:34:27 +0100

    Clover report added

--- report/html/treemap-dash-json.js
+++ report/html/treemap-dash-json.js
@@ -1,4 +1,4 @@
-var treeMapJson = {"id":"Clover database Tue Mar 24 2015 22:10:08 CET0","name":"","data":{
+var treeMapJson = {"id":"Clover database Tue Mar 24 2015 22:33:04 CET0","name":"","data":{
     "$area":10.0,"$color":60.000004,"title":" 10 Elements, 60% Coverage"},
   "children":[{"id":"main6","name":"main","data":{"$area":10.0,"$color":
         60.000004,"title":"main 10 Elements, 60% Coverage"},"children":[]}]}
