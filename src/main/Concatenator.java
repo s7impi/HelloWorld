8439fe2ed3598377799e2f38ef01532d2edc6c46
vitalik <vx0@yandex.ru>
2015-03-24 16:25:28 +0100

    Untestsed method added

--- src/main/Concatenator.java
+++ src/main/Concatenator.java
@@ -8,4 +8,5 @@ public class Concatenator {
     public String concatenate(String one, String two){
         return one + two;
     }
+    public String concatenateWithPlus(String one, String two) { return one+"+"+two; }
 }
