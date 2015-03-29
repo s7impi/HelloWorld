08688af44ad3bdd0fa2059f6eba2d7f45f9dbde5
MezVM <matmaz180@gmail.com>
2015-03-29 20:21:22 +0200

    jak cos to wywalic

--- report/html/treemap-dash-json.js
+++
@@ -1,6 +0,0 @@
-var treeMapJson = {"id":"Clover database Fri Mar 27 2015 19:15:03 CET0","name":"","data":{
-    "$area":10.0,"$color":60.000004,"title":" 10 Elements, 60% Coverage"},
-  "children":[{"id":"main6","name":"main","data":{"$area":10.0,"$color":
-        60.000004,"title":"main 10 Elements, 60% Coverage"},"children":[]}]}
-;
-processTreeMapDashJson (treeMapJson);
