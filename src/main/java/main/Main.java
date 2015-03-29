21a5094984b02660d5b6ea1749a41b1a4b4e1b22
s7impi <s7impi@gmail.com>
2015-03-30 00:32:04 +0200

    Catalog tree refactored and pom.xml edited

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        Concatenator concatenator = new Concatenator();
+        main.Concatenator concatenator = new Concatenator();
         String output = concatenator.concatenate("Hello ", "world!");
         System.out.println(output);
     }
