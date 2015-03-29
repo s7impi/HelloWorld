0a6695ca01a2af592fc56b5eb9bd790f33c7df10
mezjo <matmaz180@gmail.com>
2015-03-29 23:03:20 +0200

    odzysk

--- report/html/main/Concatenator.js
+++ report/html/main/Concatenator.js
@@ -1,10 +1,10 @@
 var clover = new Object();
 
 // JSON: {classes : [{name, id, sl, el,  methods : [{sl, el}, ...]}, ...]}
-clover.pageData = {"classes":[{"el":17,"id":16,"methods":[{"el":10,"sc":5,"sl":8},{"el":13,"sc":5,"sl":11},{"el":16,"sc":5,"sl":14}],"name":"Concatenator","sl":6}]}
+clover.pageData = {"classes":[{"el":17,"id":10,"methods":[{"el":10,"sc":5,"sl":8},{"el":13,"sc":5,"sl":11},{"el":16,"sc":5,"sl":14}],"name":"Concatenator","sl":6}]}
 
 // JSON: {test_ID : {"methods": [ID1, ID2, ID3...], "name" : "testXXX() void"}, ...};
-clover.testTargets = {"test_0":{"methods":[{"sl":8}],"name":"testConcatenate","pass":true,"statements":[{"sl":9}]},"test_1":{"methods":[{"sl":8}],"name":"testConcatenate","pass":true,"statements":[{"sl":9}]}}
+clover.testTargets = {}
 
 // JSON: { lines : [{tests : [testid1, testid2, testid3, ...]}, ...]};
-clover.srcFileLines = [[], [], [], [], [], [], [], [], [0, 1], [0, 1], [], [], [], [], [], [], [], []]
+clover.srcFileLines = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
--- report/html/treemap-dash-json.js
+++ report/html/treemap-dash-json.js
@@ -1,4 +1,4 @@
-var treeMapJson = {"id":"Clover database Sun Mar 29 2015 21:33:30 CEST0","name":"","data":{
+var treeMapJson = {"id":"Clover database Fri Mar 27 2015 19:15:03 CET0","name":"","data":{
     "$area":10.0,"$color":60.000004,"title":" 10 Elements, 60% Coverage"},
   "children":[{"id":"main6","name":"main","data":{"$area":10.0,"$color":
         60.000004,"title":"main 10 Elements, 60% Coverage"},"children":[]}]}
