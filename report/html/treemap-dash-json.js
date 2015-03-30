d5370a1c6a6fd14704eff440df5d5fa4228388ee
vitalik <vx0@yandex.ru>
2015-03-30 10:30:37 +0200

    PP-1 #comment Cloverage report added

--- report/html/treemap-dash-json.js
+++ report/html/treemap-dash-json.js
@@ -1,6 +1,6 @@
-var treeMapJson = {"id":"Clover database Mon Mar 30 2015 09:42:32 CEST0","name":"","data":{
-    "$area":10.0,"$color":0.0,"title":" 10 Elements, 0% Coverage"},
+var treeMapJson = {"id":"Clover database Mon Mar 30 2015 10:24:15 CEST0","name":"","data":{
+    "$area":10.0,"$color":60.000004,"title":" 10 Elements, 60% Coverage"},
   "children":[{"id":"main0","name":"main","data":{"$area":10.0,"$color":
-        0.0,"title":"main 10 Elements, 0% Coverage"},"children":[]}]}
+        60.000004,"title":"main 10 Elements, 60% Coverage"},"children":[]}]}
 ;
 processTreeMapDashJson (treeMapJson);
