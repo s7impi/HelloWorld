477c9124ad2d1197e31b1eb169f2502a44257485
asmolik <asmolik@users.noreply.github.com>
2015-05-08 22:52:16 +0200

    registerservice fix

--- src/main/java/services/RegisterService.java
+++ src/main/java/services/RegisterService.java
@@ -1,15 +1,26 @@
 package services;
 
+import model.dao.UserDao;
+import model.entity.User;
+
+import javax.annotation.security.PermitAll;
+import javax.inject.Inject;
 import javax.ws.rs.*;
 
 /**
  * @author asmolik
  */
 @Path("register")
+@PermitAll
 public class RegisterService {
-    @POST
-    public void register(@FormParam("mail") String mail, @FormParam("password") String password,
-                         @FormParam("name") String name) {
+    @Inject
+    UserDao dao;
 
+    @POST
+    public String register(@FormParam("mail") String mail, @FormParam("password") String password,
+                         @FormParam("name") String name) {
+        User user = new User(mail, password);
+        dao.save(user);
+        return "ok";
     }
 }
