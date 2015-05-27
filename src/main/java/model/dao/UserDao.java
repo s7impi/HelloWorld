c89abfca40df5adee3ebd06a6b937edc877f943b
mezjo <matmaz180@gmail.com>
2015-05-27 21:28:01 +0200

    wstepne konfiguracjie hibernate dzialajace narazie bez EntityManager

--- src/main/java/model/dao/UserDao.java
+++ src/main/java/model/dao/UserDao.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import model.entity.User;
+import org.hibernate.Session;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
@@ -20,5 +21,22 @@ public class UserDao {
 
     public void save(User user) {
         em.persist(user);
+    }
+
+    /**
+     * Funkcja przeznaczona do testowania dzialania EntityMenagera i generalnie
+     * hibernate'a. Mozna pisac tu glupoty.
+     */
+    public void test()
+    {
+        User us = new User();
+        us.setEmail("ccc");
+        us.setPassword("bbb");
+//        Session session = HibernateUtil.getSessionFactory().openSession();
+//        session.beginTransaction();
+//        session.save(us);
+//        session.getTransaction().commit();
+        em.persist(us);
+
     }
 }
--- src/main/java/services/HelloService.java
+++ src/main/java/services/HelloService.java
@@ -4,6 +4,7 @@ import model.dao.UserDao;
 import model.entity.User;
 
 import javax.annotation.security.PermitAll;
+import javax.persistence.EntityManager;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -19,11 +20,9 @@ public class HelloService {
     @GET
     @Produces("text/plain")
     public String hello(@Context SecurityContext sc) {
-        if (sc.isUserInRole("Customer")) {
-            return "Hello customer " + sc.getUserPrincipal();
-        } else if (sc.isUserInRole("admin")){
-            return "Hello admin " + sc.getUserPrincipal();
-        }
-        return "Hello, World!!";
+        UserDao dao = new UserDao();
+        dao.test();
+
+        return "done";
     }
 }
