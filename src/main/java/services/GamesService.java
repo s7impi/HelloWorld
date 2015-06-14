6ef5d5f1c98e811782f7dfe6704d0f1b30d08ba6
s7impi <s7impi@gmail.com>
2015-06-15 01:21:14 +0200

    Game service reimplemented

--- src/main/java/services/GamesService.java
+++ src/main/java/services/GamesService.java
@@ -1,13 +1,11 @@
 package services;
 
 import model.client.Game;
+import model.dao.GameDAO;
+import model.entity.Games;
 
 import javax.ejb.Stateless;
 import javax.inject.Named;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import javax.persistence.PersistenceUnit;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -15,7 +13,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Logger;
 
 /**
  * @author asmolik
@@ -23,30 +20,14 @@ import java.util.logging.Logger;
 
 @Stateless
 @Named
-@Path("/games")
+@Path("games/{gameName}")
 public class GamesService {
-
-    private List<Game> allGames;
-    private static final Logger logger = Logger.getLogger("services.GamesService");
-
-    @PersistenceUnit(name = "persistenceUnit")
-    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("persistenceUnit");
 
     @GET
     @Produces({MediaType.APPLICATION_JSON})
-    public List<Game> getAllGames() {
-        EntityManager em = emf.createEntityManager();
-        logger.info("Calling getAllGames");
-        this.allGames = (List<Game>)
-                em.createNamedQuery("model.entity.Games.getAllGames").getResultList();
-        if (this.allGames == null) {
-            logger.warning("No games available!");
-        }
-        em.close();
-        return this.allGames;
-    }
-
-    public void setAllGames(List<Game> allGames) {
-        this.allGames = allGames;
+    public List<Games> getGame(@PathParam("gameName") String gameName) {
+        GameDAO dao = new GameDAO();
+        List<Games> game = dao.findGameByName(gameName);
+        return game;
     }
 }
